b60206130fd34260c94fb7620d279aebd85141e4
arslankaleem7229 <arslankaleem7229@gmail.com>
2021-04-03 23:04:07 +0500

    Checking how to push from vscode

---
+++ lib/screens/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+class ReadWriteFiles extends StatefulWidget {
+  @override
+  _ReadWriteFilesState createState() => _ReadWriteFilesState();
+}
+
+class _ReadWriteFilesState extends State<ReadWriteFiles> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
